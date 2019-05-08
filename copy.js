9deeb53583a7fc2ac3210e7f3413c3975e67e76d
赵宏健 <757566833@qq.com>
2019-05-08 13:51:06 +0800

    创建项目

---
+++ copy.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const copydir  = require('copy-dir')
+try {
+    fs.mkdirSync(path.join(__dirname,'dist','config'))
+} catch (error) {
+    
+}
+copydir.sync('./config', './dist/config');
