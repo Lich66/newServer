a1a4968a687723e27e0ea237bb04ffad48015d52
赵宏健 <757566833@qq.com>
2019-05-21 11:16:02 +0800

    解析函数

--- app/util/gameUitl.ts
+++ app/util/gameUitl.ts
@@ -104,20 +104,52 @@ export class GameUitl {
     public static parseChangeConfigInRoom(config: IChange) {
         const NUMBER11 = 11;
         const NUMBER12 = 12;
+        const NUMBER13 = 13;
+        const NUMBER14 = 14;
+        const NUMBER15 = 15;
+        const NUMBER16 = 16;
+        const NUMBER17 = 17;
+        const NUMBER18 = 18;
+        const NUMBER19 = 19;
+        const NUMBER20 = 20;
+        const NUMBER21 = 21;
+        const NUMBER22 = 22;
+        const NUMBER23 = 23;
+        const NUMBER24 = 24;
+        const NUMBER25 = 25;
+        const NUMBER26 = 26;
         // [
-        //     0,   玩法
-        //     0,   人数
-        //     0,   底分
-        //     0,   局数
-        //     0,   房费
-        //     0,   游戏开始方式
-        //     0,   推注选项
-        //     0,   最大抢庄
-        //     0,   翻倍规则
-        //     0,   特殊牌型 十六进制
-        //     0,   高级选项  十六进制
-        //     0,   王赖玩法
-        //     0,   玩法类型
+        //     0,   玩法 0
+        //     0,   人数  1
+        //     0,   底分  2
+        //     0,   局数  3
+        //     0,   房费  4 
+        //     0,   游戏开始方式  5
+        //     0,   推注选项  6 
+        //     0,   最大抢庄  7 
+        //     0,   翻倍规则  8
+        //     0,   特殊牌型 十六进制  9
+        //     0,   高级选项  十六进制  10
+        //     0,   王赖玩法  11
+        //     0,   玩法类型  12
+
+
+
+        //     'string' 茶楼名字 13
+        //         审核开关  14
+        // 积分是否可查看标识  15 
+        // 打烊标志  16
+        // 隐私标志  17 
+        // 非aa支付开关  18 
+        // 参加分数  19 
+        // 参加抢庄分数  20 
+        // 负分设置    21 
+        // 积分调整选项  22
+        // 积分权限类型  23
+        // 表情赠送对象  24 
+        // 表情赠送次数  25 
+        // 赠送积分  26
+
         // ]
         const type = config[NUMBER12];
         const name = defaultClubName[type];
@@ -134,8 +166,22 @@ export class GameUitl {
             special_card_type: config[9],
             advanced_options: config[10],
             laizi_type: config[NUMBER11],
-            type,
-            name
+            type: config[NUMBER12],
+            name: config[NUMBER13],
+
+            audit_flag: config[NUMBER14],
+            integral_flag: config[NUMBER15],
+            open_flag: config[NUMBER16],
+            privacy_flag: config[NUMBER17],
+            pay_flag: config[NUMBER18],
+            join_points: config[NUMBER19],
+            join_rob_banker: config[NUMBER20],
+            point_setflag: config[NUMBER21],
+            point_adjustflag: config[NUMBER22],
+            point_permission: config[NUMBER23],
+            present_target: config[NUMBER24],
+            present_times: config[NUMBER25],
+            present_points: config[NUMBER26]
         };
     }
 }
