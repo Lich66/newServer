e1edf6477880c5f3eafb0d2490368341868ab950
lichong <806288908@qq.com>
2019-06-04 19:18:56 +0800

    特殊牌型判断

--- app/test/testReckonPoker.ts
+++ app/test/testReckonPoker.ts
@@ -2,9 +2,9 @@ import { ReckonPoker } from '../util/reckonPoker';
 const num12 = 12;
 const num13 = 13;
 const num14 = 14;
-let cardList = [[2, 2], [2, 3], [1, 2], [2, 1], [5, 0]];
-let cardList1 = [[2, 2], [2, 3], [1, 2], [3, 2], [5, 0]];
-let cardList2 = [[2, 2], [2, 3], [1, 2], [1, 3], [5, 0]];
+let cardList = [[2, 2], [4, 2], [1, 2], [3, 2], [5, 0]];
+let cardList1 = [[2, 2], [2, 3], [1, 3], [3, 2], [5, 0]];
+let cardList2 = [[2, 2], [3, 2], [1, 2], [1, 3], [5, 0]];
 
 let cardList3 = [[2, 2], [2, 3], [1, 1], [6, 0], [5, 0]];
 let cardList4 = [[2, 2], [2, 3], [1, 3], [6, 0], [5, 0]];
@@ -12,24 +12,45 @@ let cardList5 = [[1, 3], [2, 3], [4, 3], [6, 0], [5, 0]];
 
 let cardList6 = [[3, 3], [2, 3], [1, 3], [4, 3], [2, 1]];
 let cardList7 = [[2, 3], [2, 3], [1, 4], [2, 3], [2, 4]];
+let cardList8 = [[2, 3], [2, 3], [1, 4], [2, 6], [2, 4]];
 
-// 炸弹牛验证
-let result2 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList));
-console.log('验证炸弹牛结果: ' + result2);
-let result3 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList1));
-console.log('验证炸弹牛结果: ' + result3);
-let result4 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList2));
-console.log('验证炸弹牛结果: ' + result4);
-let result5 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList3));
-console.log('验证炸弹牛结果: ' + result5);
-let result6 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList4));
-console.log('验证炸弹牛结果: ' + result6);
-let result7 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList5));
-console.log('验证炸弹牛结果: ' + result7);
-let result8 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList6));
-console.log('验证炸弹牛结果: ' + result8);
+// 葫芦牛验证
+let result2 = ReckonPoker.gourdBull(ReckonPoker.sortCardList(cardList));
+console.log('验证葫芦牛结果: ' + result2);
+let result3 = ReckonPoker.gourdBull(ReckonPoker.sortCardList(cardList1));
+console.log('验证葫芦牛结果: ' + result3);
+let result4 = ReckonPoker.gourdBull(ReckonPoker.sortCardList(cardList2));
+console.log('验证葫芦牛结果: ' + result4);
+let result5 = ReckonPoker.gourdBull(ReckonPoker.sortCardList(cardList3));
+console.log('验证葫芦牛结果: ' + result5);
+let result6 = ReckonPoker.gourdBull(ReckonPoker.sortCardList(cardList4));
+console.log('验证葫芦牛结果: ' + result6);
+let result7 = ReckonPoker.gourdBull(ReckonPoker.sortCardList(cardList5));
+console.log('验证葫芦牛结果: ' + result7);
+let result8 = ReckonPoker.gourdBull(ReckonPoker.sortCardList(cardList6));
+console.log('验证葫芦牛结果: ' + result8);
 let result9 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList7));
-console.log('验证炸弹牛结果: ' + result9);
+console.log('验证葫芦牛结果: ' + result9);
+let result10 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList8));
+console.log('验证葫芦牛结果: ' + result10);
+
+// // 炸弹牛验证
+// let result2 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList));
+// console.log('验证炸弹牛结果: ' + result2);
+// let result3 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList1));
+// console.log('验证炸弹牛结果: ' + result3);
+// let result4 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList2));
+// console.log('验证炸弹牛结果: ' + result4);
+// let result5 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList3));
+// console.log('验证炸弹牛结果: ' + result5);
+// let result6 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList4));
+// console.log('验证炸弹牛结果: ' + result6);
+// let result7 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList5));
+// console.log('验证炸弹牛结果: ' + result7);
+// let result8 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList6));
+// console.log('验证炸弹牛结果: ' + result8);
+// let result9 = ReckonPoker.bombBull(ReckonPoker.sortCardList(cardList7));
+// console.log('验证炸弹牛结果: ' + result9);
 
 // // 五小牛验证
 // let result2 = ReckonPoker.fiveCalves(ReckonPoker.sortCardList(cardList));
--- app/util/reckonPoker.ts
+++ app/util/reckonPoker.ts
@@ -1,3 +1,7 @@
+const num20 = 20;
+const num30 = 30;
+const num40 = 40;
+
 export class ReckonPoker {
     /**
      * 升序排列手牌(王最小)
@@ -14,24 +18,23 @@ export class ReckonPoker {
         return cardList;
     }
 
-    // 同花顺>五小牛>炸弹牛>四十牛>葫芦牛>同花牛>顺子牛>牛牛>牛九>牛八>牛七>牛六>牛五>牛四>牛三>牛二>牛一>无牛
-
     /**
      * 同花顺判断逻辑
+     * 同花+顺子
      * @param cardList 排序过后的玩家的手牌
      */
     public static straightFlush(cardList: number[][]): boolean {
         let len = cardList.length - 1;
         // 是否有非癞子的相同牌
         for (let i = 0; i < len; i++) {
-            if (cardList[i][1] === cardList[i + 1][1] && cardList[i][1] !== 0) {
+            if (cardList[i][1] !== 0 && cardList[i][1] === cardList[i + 1][1]) {
                 console.log('同花顺: 有两张相同的非癞子牌!');
                 return false;
             }
         }
         // 是否有非癞子的不相同花色
         for (let i = 0; i < len; i++) {
-            if (cardList[i][0] !== cardList[i + 1][0] && cardList[i][1] !== 0) {
+            if (cardList[i][1] !== 0 && cardList[i][0] !== cardList[i + 1][0]) {
                 console.log('同花顺: 有两张花色不同的非癞子牌!');
                 return false;
             }
@@ -52,12 +55,13 @@ export class ReckonPoker {
 
     /**
      * 五小牛判断逻辑
+     * 五张之和小于等于10
      * @param cardList 排序过后的玩家的手牌
      */
     public static fiveCalves(cardList: number[][]): boolean {
         let len = cardList.length;
-        let sum = 0;
-        let count = 0;
+        let sum = 0;    // 总点数
+        let count = 0;  // 癞子个数
         for (let i = 0; i < len; i++) {
             if (cardList[i][1] !== 0) {
                 sum += cardList[i][1];
@@ -82,11 +86,12 @@ export class ReckonPoker {
 
     /**
      * 炸弹牛判断逻辑
+     * 四张一样的炸弹
      * @param cardList 排序过后的玩家的手牌
      */
     public static bombBull(cardList: number[][]): boolean {
-        let map = {};
-        let count = 0;
+        let map = {};   // 牌值:张数
+        let count = 0;  // 癞子个数
         for (const iterator of cardList) {
             if (iterator[1] === 0) {
                 count++;
@@ -98,7 +103,7 @@ export class ReckonPoker {
                 map[iterator[1]] = 1;
             }
         }
-        console.log('炸弹牛: 统计后的[牌值:个数] = ' + JSON.stringify(map));
+        console.log('炸弹牛: 统计后的[牌值:张数] = ' + JSON.stringify(map));
         let keys = Object.keys(map);
         if (keys.length > 2) {
             console.log('炸弹牛: 非癞子牌种类>2,不能构成炸弹');
@@ -113,7 +118,112 @@ export class ReckonPoker {
         return true;
     }
 
-    public static FortyBull() {
-
+    /**
+     * 四十牛判断逻辑
+     * 五张之和大于等于40
+     * @param cardList 排序过后的玩家的手牌(里面没有JQK)
+     */
+    public static FortyBull(cardList: number[][]): boolean {
+        let num = 0;    // 总点数
+        let count = 0;  // 癞子个数
+        for (const iterator of cardList) {
+            if (iterator[1] === 0) {
+                count++;
+                continue;
+            }
+            num += iterator[1];
+        }
+        if (num >= num40) {
+            return true;
+        }
+        if (count === 1 && num >= num30) {
+            return true;
+        }
+        if (count === 2 && num >= num20) {
+            return true;
+        }
+        return false;
     }
+
+    /**
+     * 葫芦牛判断逻辑
+     * 对子+三张
+     * @param cardList 排序过后的玩家的手牌
+     */
+    public static gourdBull(cardList: number[][]): boolean {
+        let map = {};
+        let count = 0;
+        for (const iterator of cardList) {
+            if (iterator[1] === 0) {
+                count++;
+                continue;
+            }
+            if (map.hasOwnProperty(iterator[1])) {
+                map[iterator[1]]++;
+            } else {
+                map[iterator[1]] = 1;
+            }
+        }
+        console.log('葫芦牛: 统计后的[牌值:个数] = ' + JSON.stringify(map));
+        let keys = Object.keys(map);
+        if (keys.length > 2) {
+            console.log('葫芦牛: 非癞子牌种类>2,不能构成葫芦牛');
+            return false;
+        }
+        if (keys.length === 2) {
+            if ((map[keys[0]] === 1 && map[keys[1]] === 4) || (map[keys[0]] === 4 && map[keys[1]] === 1)) {
+                console.log('葫芦牛: 非癞子牌种类=2,没有对子(1:4),不能构成葫芦牛');
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 同花牛判断逻辑
+     * 同花
+     * @param cardList 排序过后的玩家的手牌
+     */
+    public static flushBull(cardList: number[][]): boolean {
+        let len = cardList.length - 1;
+        // 是否有非癞子的不相同花色
+        for (let i = 0; i < len; i++) {
+            if (cardList[i][1] !== 0 && cardList[i][0] !== cardList[i + 1][0]) {
+                console.log('同花牛: 有两张花色不同的非癞子牌!');
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 顺子牛判断逻辑
+     * 顺子
+     * @param cardList 排序过后的玩家的手牌
+     */
+    public static straightBull(cardList: number[][]): boolean {
+        let len = cardList.length - 1;
+        // 是否有非癞子的相同牌
+        for (let i = 0; i < len; i++) {
+            if (cardList[i][1] !== 0 && cardList[i][1] === cardList[i + 1][1]) {
+                console.log('顺子牛: 有两张相同的非癞子牌!');
+                return false;
+            }
+        }
+        // 是否能构成顺子(相邻非癞子两牌的值相差之和不超过4)
+        let num = 0;
+        for (let i = 0; i < len; i++) {
+            if (cardList[i][1] !== 0 && cardList[i + 1][1] !== 0) {
+                num += (cardList[i + 1][1] - cardList[i][1]);
+            }
+            if (num > 4) {
+                console.log('顺子牛: 构不成顺子!');
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 同花顺>五小牛>炸弹牛>四十牛>葫芦牛>同花牛>顺子牛>牛牛>牛九>牛八>牛七>牛六>牛五>牛四>牛三>牛二>牛一>无牛
+
 }
