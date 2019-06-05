df7e9846021e00293a5049b8b07cdc1f02c3e3d4
赵宏健 <757566833@qq.com>
2019-06-05 11:54:47 +0800

    充值模拟延迟

--- app/servers/user/handler/userHandler.ts
+++ app/servers/user/handler/userHandler.ts
@@ -16,10 +16,34 @@ export class Handler {
     public async addDiamond(msg: { diamond: number }, session: BackendSession) {
         const user = await User.getUser({ userid: Number.parseInt(session.uid, 0) });
         const diamond = user.diamond + msg.diamond;
-        const nuser = await User.updateUser({ userid: Number.parseInt(session.uid, 0) }, { diamond });
-        return {
-            code: 0,
-            data: nuser
-        };
+        const bool = await this.timeout();
+        if (bool) {
+            const nuser = await User.updateUser({ userid: Number.parseInt(session.uid, 0) }, { diamond });
+            return {
+                code: 0,
+                data: nuser
+            };
+        } else {
+            return {
+                code: 165165816
+                // data: nuser
+            };
+        }
+
+    }
+
+    public async timeout() {
+        const time = 5000;
+        return new Promise(function (resolve, reject) {
+            // ... some code
+
+            if (true) {
+                setTimeout(() => {
+                    resolve(true);
+                }, time);
+
+            } else {
+            }
+        });
     }
 }
